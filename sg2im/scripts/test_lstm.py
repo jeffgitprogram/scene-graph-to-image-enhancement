4317b1d2748a5279547b128b9e03ce459e6ebc20
jeffery <shiningfinger29@gmail.com>
2019-04-09 22:40:03 -0700

    Now the new class load caption information from the json file and build vocab from that

---
+++ sg2im/scripts/test_lstm.py
@@ -0,0 +1,160 @@
+import argparse
+import functools
+import os
+import json
+import math
+from collections import defaultdict
+import random
+import io
+
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+
+import warnings
+warnings.filterwarnings('ignore')
+import mxnet as mx
+from mxnet import gluon
+import gluonnlp as nlp
+
+from sg2im.data import imagenet_deprocess_batch
+from sg2im.data.coco_caption import CocoCaptionDataSet,coco_caption_collate_fn
+from sg2im.utils import int_tuple, float_tuple, str_tuple
+from sg2im.utils import timeit, bool_flag, LossManager
+COCO_DIR = os.path.expanduser('../datasets/coco')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', default='coco', choices=['vg', 'coco'])
+
+# Optimization hyperparameters
+parser.add_argument('--batch_size', default=32, type=int)
+parser.add_argument('--num_iterations', default=1000000, type=int)
+parser.add_argument('--learning_rate', default=1e-4, type=float)
+
+# Switch the generator to eval mode after this many iterations
+parser.add_argument('--eval_mode_after', default=100000, type=int)
+
+# Dataset options common to both VG and COCO
+parser.add_argument('--image_size', default='64,64', type=int_tuple)
+parser.add_argument('--num_train_samples', default=None, type=int)
+parser.add_argument('--num_val_samples', default=1024, type=int)
+parser.add_argument('--shuffle_val', default=True, type=bool_flag)
+parser.add_argument('--loader_num_workers', default=4, type=int)
+parser.add_argument('--include_relationships', default=True, type=bool_flag)
+
+
+# COCO-specific options
+parser.add_argument('--coco_train_image_dir',
+         default=os.path.join(COCO_DIR, 'images/train2017'))
+parser.add_argument('--coco_val_image_dir',
+         default=os.path.join(COCO_DIR, 'images/val2017'))
+parser.add_argument('--coco_train_captions_json',
+         default=os.path.join(COCO_DIR, 'annotations/captions_train2017.json'))
+parser.add_argument('--coco_train_instances_json',
+         default=os.path.join(COCO_DIR, 'annotations/instances_train2017.json'))
+parser.add_argument('--coco_train_stuff_json',
+         default=os.path.join(COCO_DIR, 'annotations/stuff_train2017.json'))
+parser.add_argument('--coco_val_captions_json',
+         default=os.path.join(COCO_DIR, 'annotations/captions_val2017.json'))
+parser.add_argument('--coco_val_instances_json',
+         default=os.path.join(COCO_DIR, 'annotations/instances_val2017.json'))
+parser.add_argument('--coco_val_stuff_json',
+         default=os.path.join(COCO_DIR, 'annotations/stuff_val2017.json'))
+parser.add_argument('--instance_whitelist', default=None, type=str_tuple)
+parser.add_argument('--stuff_whitelist', default=None, type=str_tuple)
+parser.add_argument('--coco_include_other', default=False, type=bool_flag)
+parser.add_argument('--min_object_size', default=0.02, type=float)
+parser.add_argument('--min_objects_per_image', default=3, type=int)
+parser.add_argument('--coco_stuff_only', default=True, type=bool_flag)
+
+
+# Output options
+parser.add_argument('--print_every', default=10, type=int)
+parser.add_argument('--timing', default=False, type=bool_flag)
+parser.add_argument('--checkpoint_every', default=10000, type=int)
+parser.add_argument('--output_dir', default=os.getcwd())
+parser.add_argument('--checkpoint_name', default='checkpoint')
+parser.add_argument('--checkpoint_start_from', default=None)
+parser.add_argument('--restore_from_checkpoint', default=False, type=bool_flag)
+
+# Generator options
+parser.add_argument('--mask_size', default=16, type=int) # Set this to 0 to use no masks
+parser.add_argument('--embedding_dim', default=128, type=int)
+parser.add_argument('--gconv_dim', default=128, type=int)
+parser.add_argument('--gconv_hidden_dim', default=512, type=int)
+parser.add_argument('--gconv_num_layers', default=5, type=int)
+parser.add_argument('--mlp_normalization', default='none', type=str)
+parser.add_argument('--refinement_network_dims', default='1024,512,256,128,64', type=int_tuple)
+parser.add_argument('--normalization', default='batch')
+parser.add_argument('--activation', default='leakyrelu-0.2')
+parser.add_argument('--layout_noise_dim', default=32, type=int)
+parser.add_argument('--use_boxes_pred_after', default=-1, type=int)
+def build_coco_caption_dsets(args):
+  dset_kwargs = {
+    'image_dir': args.coco_train_image_dir,
+    'captions_json': args.coco_train_captions_json,
+    'instances_json': args.coco_train_instances_json,
+    'stuff_json': args.coco_train_stuff_json,
+    'stuff_only': args.coco_stuff_only,
+    'image_size': args.image_size,
+    'mask_size': args.mask_size,
+    'max_samples': args.num_train_samples,
+    'min_object_size': args.min_object_size,
+    'min_objects_per_image': args.min_objects_per_image,
+    'instance_whitelist': args.instance_whitelist,
+    'stuff_whitelist': args.stuff_whitelist,
+    'include_other': args.coco_include_other,
+    'include_relationships': args.include_relationships,
+  }
+  train_dset = CocoCaptionDataSet(**dset_kwargs)
+  num_objs = train_dset.total_objects()
+  num_imgs = len(train_dset)
+  print('Training dataset has %d images and %d objects' % (num_imgs, num_objs))
+  print('(%.2f objects per image)' % (float(num_objs) / num_imgs))
+
+  # dset_kwargs['image_dir'] = args.coco_val_image_dir
+  # dset_kwargs['captions_json'] = args.coco_val_captions_json
+  # dset_kwargs['instances_json'] = args.coco_val_instances_json
+  # dset_kwargs['stuff_json'] = args.coco_val_stuff_json
+  # dset_kwargs['max_samples'] = args.num_val_samples
+  # val_dset = CocoCaptionDataSet(**dset_kwargs)
+
+
+  return train_dset
+
+
+def build_caption_loaders(args):
+
+    train_dset = build_coco_caption_dsets(args)
+    collate_fn = coco_caption_collate_fn
+
+    loader_kwargs = {
+        'batch_size': args.batch_size,
+        'num_workers': args.loader_num_workers,
+        'shuffle': True,
+        'batchify_fn': collate_fn,
+    }
+    train_loader = nlp.data.ShardedDataLoader(train_dset, **loader_kwargs)
+    counter, vocab = train_dset.getCounterandVocab()
+    #loader_kwargs['shuffle'] = args.shuffle_val
+    #val_loader = nlp.data.dataloader(val_dset, **loader_kwargs)
+    return train_loader,counter,vocab#, val_loader
+def main(args):
+    print("start")
+    print(args)
+    num_gpus = 1
+    context = [mx.gpu(i) for i in range(num_gpus)] if num_gpus else [mx.cpu()]
+    #train_dset = build_coco_caption_dsets(args)
+    train_loader,counter,vocab = build_caption_loaders(args)
+    awd_model_name = 'awd_lstm_lm_1150'
+    awd_model,vocab = nlp.model.get_model(awd_model_name,)
+    for batch in train_loader:
+        img,captions= batch
+
+
+
+if __name__ == '__main__':
+  args = parser.parse_args()
+  main(args)
--- sg2im/sg2im/data/coco_caption.py
+++ sg2im/sg2im/data/coco_caption.py
@@ -1,4 +1,4 @@
-import json, os, random, math
+import json, os, random, math, io
 from collections import defaultdict
 
 import torch
@@ -9,87 +9,125 @@ import numpy as np
 import PIL
 from skimage.transform import resize as imresize
 import pycocotools.mask as mask_utils
+import warnings
+warnings.filterwarnings('ignore')
+
+import mxnet as mx
+from mxnet import gluon
+import gluonnlp as nlp
 
 from .utils import imagenet_preprocess, Resize
 
 class CocoCaptionDataSet(Dataset):
-    def _init_(self, image_dir, captions_json,instances_json, stuff_json=None,
+  def __init__(self, image_dir,captions_json,instances_json, stuff_json=None,
                stuff_only=True, image_size=(64, 64), mask_size=16,
                normalize_images=True, max_samples=None,
                include_relationships=True, min_object_size=0.02,
                min_objects_per_image=3, max_objects_per_image=8,
                include_other=False, instance_whitelist=None, stuff_whitelist=None):
-        super(Dataset,self).__init__()
+    super(Dataset,self).__init__()
 
-        if stuff_only and stuff_json is None:
-            print('WARNING: Got stuff_only=True but stuff_json=None.')
-            print('Falling back to stuff_only=False.')
+    if stuff_only and stuff_json is None:
+        print('WARNING: Got stuff_only=True but stuff_json=None.')
+        print('Falling back to stuff_only=False.')
 
 
-        self.image_dir = image_dir
-        self.mask_size = mask_size
-        self.max_samples = max_samples
-        self.normalize_images = normalize_images
-        self.include_relationships = include_relationships
-        self.set_image_size(image_size)
+    self.image_dir = image_dir
+    self.mask_size = mask_size
+    self.max_samples = max_samples
+    self.normalize_images = normalize_images
+    self.include_relationships = include_relationships
+    self.set_image_size(image_size)
+    self.tokenizer = nlp.data.NLTKMosesTokenizer()
 
-        captions_data = None
-        if captions_json is not None and captions_json != '':
-            with open(captions_json,'r') as f:
-                captions_data = json.load(f)
+    captions_data = None
+    if captions_json is not None and captions_json != '':
+        with open(captions_json,'r') as f:
+            captions_data = json.load(f)
 
-        with open(instances_json, 'r') as f:
-            instances_data = json.load(f)
+    with open(instances_json, 'r') as f:
+        instances_data = json.load(f)
 
-        stuff_data = None
-        if stuff_json is not None and stuff_json != '':
-            with open(stuff_json, 'r') as f:
-                stuff_data = json.load(f)
+    stuff_data = None
+    if stuff_json is not None and stuff_json != '':
+        with open(stuff_json, 'r') as f:
+            stuff_data = json.load(f)
 
-        self.image_ids = []
-        self.image_id_to_filename = {}
-        self.image_id_to_size = {}
-        for image_data in instances_data['images']:
-            image_id = image_data['id']
-            filename = image_data['file_name']
-            width = image_data['width']
-            height = image_data['height']
-            self.image_ids.append(image_id)
-            self.image_id_to_filename[image_id] = filename
-            self.image_id_to_size[image_id] = (width, height)
+    self.image_ids = []
+    self.image_id_to_filename = {}
+    self.image_id_to_size = {}
+    for image_data in instances_data['images']:
+        image_id = image_data['id']
+        filename = image_data['file_name']
+        width = image_data['width']
+        height = image_data['height']
+        self.image_ids.append(image_id)
+        self.image_id_to_filename[image_id] = filename
+        self.image_id_to_size[image_id] = (width, height)
 
-        self.vocab = {
-            'object_name_to_idx': {},
-            #'pred_name_to_idx': {},
-        }
+    self.vocab = {
+        'object_name_to_idx': {},
+        #'pred_name_to_idx': {},
+    }
 
-        object_idx_to_name = {}
-        all_instance_categories = []
-        for category_data in instances_data['categories']:
-            category_id = category_data['id']
+    object_idx_to_name = {}
+    all_instance_categories = []
+    for category_data in instances_data['categories']:
+        category_id = category_data['id']
+        category_name = category_data['name']
+        all_instance_categories.append(category_name)
+        object_idx_to_name[category_id] = category_name
+        self.vocab['object_name_to_idx'][category_name] = category_id
+    all_stuff_categories = []
+    if stuff_data:
+        for category_data in stuff_data['categories']:
             category_name = category_data['name']
-            all_instance_categories.append(category_name)
+            category_id = category_data['id']
+            all_stuff_categories.append(category_name)
             object_idx_to_name[category_id] = category_name
             self.vocab['object_name_to_idx'][category_name] = category_id
-        all_stuff_categories = []
-        if stuff_data:
-            for category_data in stuff_data['categories']:
-                category_name = category_data['name']
-                category_id = category_data['id']
-                all_stuff_categories.append(category_name)
-                object_idx_to_name[category_id] = category_name
-                self.vocab['object_name_to_idx'][category_name] = category_id
 
-        if instance_whitelist is None:
-            instance_whitelist = all_instance_categories
-        if stuff_whitelist is None:
-            stuff_whitelist = all_stuff_categories
-        category_whitelist = set(instance_whitelist) | set(stuff_whitelist)
+    if instance_whitelist is None:
+        instance_whitelist = all_instance_categories
+    if stuff_whitelist is None:
+        stuff_whitelist = all_stuff_categories
+    category_whitelist = set(instance_whitelist) | set(stuff_whitelist)
 
-        # Add object data from instances
-        self.image_id_to_objects = defaultdict(list)
-        for object_data in instances_data['annotations']:
+    # Add object data from instances
+    self.image_id_to_objects = defaultdict(list)
+    for object_data in instances_data['annotations']:
+        image_id = object_data['image_id']
+        _, _, w, h = object_data['bbox']
+        W, H = self.image_id_to_size[image_id]
+        box_area = (w * h) / (W * H)
+        box_ok = box_area > min_object_size
+        object_name = object_idx_to_name[object_data['category_id']]
+        category_ok = object_name in category_whitelist
+        other_ok = object_name != 'other' or include_other
+        if box_ok and category_ok and other_ok:
+            self.image_id_to_objects[image_id].append(object_data)
+
+    self.image_id_to_captions = defaultdict(list)
+    # Add object data from stuff
+    if captions_data:
+        image_ids_with_caption = set()
+        for caption_data in captions_data['annotations']:
+            image_id = caption_data['image_id']
+            image_ids_with_caption.add(image_id)
+            caption = caption_data['caption']
+            self.image_id_to_captions[image_id].append(caption)#More than one caption data each image
+        #new_image_ids = []
+        #for image_id in self.image_ids:
+            #if image_id in image_ids_with_caption:
+                #new_image_ids.append(image_id)
+        #self.image_ids = new_image_ids # Make sure all image has captions
+
+    # Add object data from stuff
+    if stuff_data:
+        image_ids_with_stuff = set()
+        for object_data in stuff_data['annotations']:
             image_id = object_data['image_id']
+            image_ids_with_stuff.add(image_id)
             _, _, w, h = object_data['bbox']
             W, H = self.image_id_to_size[image_id]
             box_area = (w * h) / (W * H)
@@ -99,94 +137,54 @@ class CocoCaptionDataSet(Dataset):
             other_ok = object_name != 'other' or include_other
             if box_ok and category_ok and other_ok:
                 self.image_id_to_objects[image_id].append(object_data)
+        if stuff_only:
+            new_image_ids = []
+            for image_id in self.image_ids:
+                if image_id in image_ids_with_stuff:
+                    new_image_ids.append(image_id)
+            self.image_ids = new_image_ids
 
-        self.image_id_to_captions = defaultdict(list)
-        # Add object data from stuff
-        if captions_data:
-            image_ids_with_caption = set()
-            for caption_data in captions_data['annotations']:
-                image_id = caption_data['image_id']
-                image_ids_with_caption.add(image_id)
-                caption = caption_data['caption']
-                self.image_id_to_captions[image_id].append(caption_data)#More than one caption data each image
-            #new_image_ids = []
-            #for image_id in self.image_ids:
-                #if image_id in image_ids_with_caption:
-                    #new_image_ids.append(image_id)
-            #self.image_ids = new_image_ids # Make sure all image has captions
-
-        # Add object data from stuff
-        if stuff_data:
-            image_ids_with_stuff = set()
-            for object_data in stuff_data['annotations']:
-                image_id = object_data['image_id']
-                image_ids_with_stuff.add(image_id)
-                _, _, w, h = object_data['bbox']
-                W, H = self.image_id_to_size[image_id]
-                box_area = (w * h) / (W * H)
-                box_ok = box_area > min_object_size
-                object_name = object_idx_to_name[object_data['category_id']]
-                category_ok = object_name in category_whitelist
-                other_ok = object_name != 'other' or include_other
-                if box_ok and category_ok and other_ok:
-                    self.image_id_to_objects[image_id].append(object_data)
-            if stuff_only:
-                new_image_ids = []
-                for image_id in self.image_ids:
-                    if image_id in image_ids_with_stuff:
-                        new_image_ids.append(image_id)
-                self.image_ids = new_image_ids
-
-                all_image_ids = set(self.image_id_to_filename.keys())
-                image_ids_to_remove = all_image_ids - image_ids_with_stuff
-                for image_id in image_ids_to_remove:
-                    self.image_id_to_filename.pop(image_id, None)
-                    self.image_id_to_size.pop(image_id, None)
-                    self.image_id_to_objects.pop(image_id, None)
-                    self.image_id_to_captions.pop(image_id,None)
+            all_image_ids = set(self.image_id_to_filename.keys())
+            image_ids_to_remove = all_image_ids - image_ids_with_stuff
+            for image_id in image_ids_to_remove:
+                self.image_id_to_filename.pop(image_id, None)
+                self.image_id_to_size.pop(image_id, None)
+                self.image_id_to_objects.pop(image_id, None)
+                self.image_id_to_captions.pop(image_id,None)
 
 
+    # COCO category labels start at 1, so use 0 for __image__
+    self.vocab['object_name_to_idx']['__image__'] = 0
+
+    # Build object_idx_to_name
+    name_to_idx = self.vocab['object_name_to_idx']
+    assert len(name_to_idx) == len(set(name_to_idx.values()))
+    max_object_idx = max(name_to_idx.values())
+    idx_to_name = ['NONE'] * (1 + max_object_idx)
+    for name, idx in self.vocab['object_name_to_idx'].items():
+        idx_to_name[idx] = name
+    self.vocab['object_idx_to_name'] = idx_to_name
+
+    # Prune images that have too few or too many objects
+    new_image_ids = []
+    total_objs = 0
+    for image_id in self.image_ids:
+        num_objs = len(self.image_id_to_objects[image_id])
+        total_objs += num_objs
+        if min_objects_per_image <= num_objs <= max_objects_per_image:
+            new_image_ids.append(image_id)
+
+    #Keep one caption for each img
+    self.image_ids = new_image_ids
+    new_img_to_captions = defaultdict(list)
+    for image_id in self.image_ids:
+        captions = [cap for cap in self.image_id_to_captions[image_id]]
+        caption = random.choice(captions)
+        new_img_to_captions[image_id].append(caption)
+    self.image_id_to_captions = new_img_to_captions
 
 
-
-        self.sentences = [caption_sentences for caption_sentences in self.image_id_to_captions]
-
-        # COCO category labels start at 1, so use 0 for __image__
-        self.vocab['object_name_to_idx']['__image__'] = 0
-
-        # Build object_idx_to_name
-        name_to_idx = self.vocab['object_name_to_idx']
-        assert len(name_to_idx) == len(set(name_to_idx.values()))
-        max_object_idx = max(name_to_idx.values())
-        idx_to_name = ['NONE'] * (1 + max_object_idx)
-        for name, idx in self.vocab['object_name_to_idx'].items():
-            idx_to_name[idx] = name
-        self.vocab['object_idx_to_name'] = idx_to_name
-
-        # Prune images that have too few or too many objects
-        new_image_ids = []
-        total_objs = 0
-        for image_id in self.image_ids:
-            num_objs = len(self.image_id_to_objects[image_id])
-            total_objs += num_objs
-            if min_objects_per_image <= num_objs <= max_objects_per_image:
-                new_image_ids.append(image_id)
-        self.image_ids = new_image_ids
-
-        self.vocab['pred_idx_to_name'] = [
-            '__in_image__',
-            'left of',
-            'right of',
-            'above',
-            'below',
-            'inside',
-            'surrounding',
-        ]
-        self.vocab['pred_name_to_idx'] = {}
-        for idx, name in enumerate(self.vocab['pred_idx_to_name']):
-            self.vocab['pred_name_to_idx'][name] = idx
-
-    def set_image_size(self, image_size):
+  def set_image_size(self, image_size):
         print('called set_image_size', image_size)
         transform = [Resize(image_size), T.ToTensor()]
         if self.normalize_images:
@@ -194,7 +192,7 @@ class CocoCaptionDataSet(Dataset):
         self.transform = T.Compose(transform)
         self.image_size = image_size
 
-    def total_objects(self):
+  def total_objects(self):
         total_objs = 0
         for i, image_id in enumerate(self.image_ids):
             if self.max_samples and i >= self.max_samples:
@@ -203,12 +201,12 @@ class CocoCaptionDataSet(Dataset):
             total_objs += num_objs
         return total_objs
 
-    def __len__(self):
+  def __len__(self):
         if self.max_samples is None:
             return len(self.image_ids)
         return min(len(self.image_ids), self.max_samples)
 
-    def __getitem__(self, index):
+  def __getitem__(self, index):
         """
         Get the pixels of an image, and a random synthetic scene graph for that
         image constructed on-the-fly from its COCO object annotations. We assume
@@ -230,36 +228,41 @@ class CocoCaptionDataSet(Dataset):
         image_id = self.image_ids[index]
         filename = self.image_id_to_filename[image_id]
         image_path = os.path.join(self.image_dir, filename)
+
         with open(image_path, 'rb') as f:
             with PIL.Image.open(f) as image:
                 image = self.transform(image.convert('RGB'))
 
         captions = []
-        for caption_data in self.image_id_to_captions[image_id]:
-            captions.append(caption_data['caption'])
+        for caption in self.image_id_to_captions[image_id]:
+            caption = ['<bos>'] + self.tokenizer(caption.lower()) + ['<sos>']
+            captions.append(caption)
         return image,captions
 
-
-
-    def coco_caption_collate_fn(batch):
-        """
-        Collate function to be used when wrapping CocoSceneGraphDataset in a
-        DataLoader. Returns a tuple of the following:
-
-        - imgs: FloatTensor of shape (N, C, H, W)
-        - objs: LongTensor of shape (O,) giving object categories
-        - boxes: FloatTensor of shape (O, 4)
-        - masks: FloatTensor of shape (O, M, M)
-        - triples: LongTensor of shape (T, 3) giving triples
-        - obj_to_img: LongTensor of shape (O,) mapping objects to images
-        - triple_to_img: LongTensor of shape (T,) mapping triples to images
-        """
-        all_captions,all_imgs = [],[]
-        for i, (img,captions) in enumerate(batch):
-            all_imgs.append(img[None])
-            all_captions.append(captions)
+  def getCounterandVocab(self):
+      captions = []
+      for image_id in self.image_ids:
+          for caption in self.image_id_to_captions[image_id]:
+              caption = ['<bos>'] + self.tokenizer(caption.lower()) + ['<sos>']
+              captions.append(caption)
+      counter = nlp.data.count_tokens([word for sentence in captions for word in sentence])
+      vocab = nlp.Vocab(counter)
+      return counter,vocab
 
 
 
-        out = (all_captions,all_imgs)
-        return out
+def coco_caption_collate_fn(batch):
+    """
+    Collate function to be used when wrapping CocoSceneGraphDataset in a
+    DataLoader. Returns a tuple of the following:
+
+    """
+    all_captions,all_imgs = [],[]
+    #all_cap_to_img = []
+    for i, (img,captions) in enumerate(batch):
+        #C = len(captions)
+        all_imgs.append(img[None])
+        all_captions.append(captions)
+        #all_cap_to_img.extend(np.full(C,i))
+    out = (all_imgs,all_captions)
+    return out
