b1c91ed9eb0d69c227db626b6bfc7007197eca2a
jefferyli <shiningfinger29@gmail.com>
2019-04-19 20:22:31 -0700

    Tested output layout,modified the run_model script to adopt our changes

--- sg2im/scripts/run_model.py
+++ sg2im/scripts/run_model.py
@@ -18,15 +18,20 @@ import argparse, json, os
 
 from imageio import imwrite
 import torch
+import mxnet as mx
+from mxnet import nd
+import gluonnlp as nlp
 
 from sg2im.model import Sg2ImModel
+from sg2im.captionencoder import CaptionEncoder
 from sg2im.data.utils import imagenet_deprocess_batch
 import sg2im.vis as vis
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--checkpoint', default='sg2im-models/vg128.pt')
-parser.add_argument('--scene_graphs_json', default='scene_graphs/figure_6_sheep.json')
+parser.add_argument('--scene_graphs_json', default='scene_graphs/context.json')
+
 parser.add_argument('--output_dir', default='outputs')
 parser.add_argument('--draw_scene_graphs', type=int, default=0)
 parser.add_argument('--device', default='gpu', choices=['cpu', 'gpu'])
@@ -42,6 +47,11 @@ def main(args):
   if not os.path.isdir(args.output_dir):
     print('Output directory "%s" does not exist; creating it' % args.output_dir)
     os.makedirs(args.output_dir)
+  layout_dir_name = os.path.join(args.output_dir, 'layout')
+  if not os.path.isdir(layout_dir_name)
+    print('Layout output directory "%s" does not exist; creating it' % layout_dir_name)
+    os.makedirs(layout_dir_name)
+
 
   if args.device == 'cpu':
     device = torch.device('cpu')
@@ -59,13 +69,28 @@ def main(args):
   model.eval()
   model.to(device)
 
+  #Load the caption encoder
+  # Load Caption Encoder
+  lstm, sent_vocab = nlp.model.get_model('standard_lstm_lm_650',
+                                                   dataset_name='wikitext-2',
+                                                   pretrained=True,
+                                                   ctx=mx.cpu())
+  # print(self.sent_vocab)
+  caption_encoder = CaptionEncoder()
+  caption_encoder.embedding = lstm.embedding
+  caption_encoder.encoder = lstm.encoder
+  caption_encoder.begin_state = lstm.begin_state
+  caption_encoder.hybridize()
+  print(caption_encoder)
+
   # Load the scene graphs
   with open(args.scene_graphs_json, 'r') as f:
     scene_graphs = json.load(f)
-
+  #Generate hiddens for captions
+  caption_hiddens = generateCaptionHidden(caption_encoder, sent_vocab,scene_graphs)
   # Run the model forward
   with torch.no_grad():
-    imgs, boxes_pred, masks_pred, _ = model.forward_json(scene_graphs)
+    imgs, boxes_pred, masks_pred, _, _, objs = model.forward_json(scene_graphs,caption_hiddens)
   imgs = imagenet_deprocess_batch(imgs)
 
   # Save the generated images
@@ -73,6 +98,11 @@ def main(args):
     img_np = imgs[i].numpy().transpose(1, 2, 0)
     img_path = os.path.join(args.output_dir, 'img%06d.png' % i)
     imwrite(img_path, img_np)
+    layout_plt = vis.draw_layout(model.vocab, objs[i], boxes_pred[i], masks_pred[i], show_boxes=True)
+    layout_path = os.path.join(layout_dir_name, 'img%06dlayout.png' % i)
+    layout_plt.savefig(layout_path)
+    layout_plt.clf()
+
 
   # Draw the scene graphs
   if args.draw_scene_graphs == 1:
@@ -81,9 +111,40 @@ def main(args):
       sg_img_path = os.path.join(args.output_dir, 'sg%06d.png' % i)
       imwrite(sg_img_path, sg_img)
 
-def generateCaptionHidden(self):{
+def generateCaptionHidden(encoder, vocab, scene_graphs):
+  if isinstance(scene_graphs, dict):
+  # We just got a single scene graph, so promote it to a list
+    scene_graphs = [scene_graphs]
+  tokenizer = nlp.data.NLTKMosesTokenizer()
+  hidden_set = []
+  for i, sg in enumerate(scene_graphs):
+    sentence = tokenizer(sg['caption'].lower()) + ['<eos>']
+    print(sentence)
+    length = len(sentence)
+    sentence = vocab[sentence]
+    sentence = nd.array([sentence])
+    length = nd.array(length)
+    _, hiddens = get_features(encoder, sentence, length)
+    hiddens = hiddens[1]
+    hiddens = torch.from_numpy(hiddens.asnumpy())
+    hidden_set.append(hiddens)
+  hidden_set = torch.cat(hidden_set)
+  hidden_set = hidden_set.cuda() if args.device == 'gpu' else hidden_set.cpu()
+  return hidden_set
 
-}
+
+def get_features(self, model, data, valid_lengths):
+    # length = data.shape[1]
+    batch_size = data.shape[0]
+    hidden_state = model.begin_state(func=mx.nd.zeros, batch_size=batch_size)
+    # mask = mx.nd.arange(length).expand_dims(0).broadcast_axes(axis=(0,), size=(batch_size,))
+    # mask = mask < valid_lengths.expand_dims(1).astype('float32')
+    # print(data.shape)
+    data = mx.nd.transpose(data)
+    output, (hidden, cell) = model(data, hidden_state)
+    # hidden = mx.nd.transpose(hidden, axes=(1, 0, 2))
+    # print(hidden.shape)
+    return (output, hidden)
 if __name__ == '__main__':
   args = parser.parse_args()
   main(args)
